8bc0e9e5b1c1593e3fb44337dcc6322bfd6f532f
knockhow <knockhow@7b7bdc71-1198-495c-96f7-db1c0ecb8923>
2020-12-19 02:47:50 +0000

    First Import

---
+++ Workshop/Empty/Client/MainForm.Designer.cs
@@ -0,0 +1,252 @@
+/* ========================================================================
+ * Copyright (c) 2005-2019 The OPC Foundation, Inc. All rights reserved.
+ *
+ * OPC Foundation MIT License 1.00
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The complete license agreement can be found here:
+ * http://opcfoundation.org/License/MIT/1.00/
+ * ======================================================================*/
+
+namespace Quickstarts.EmptyClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MenuBar = new System.Windows.Forms.MenuStrip();
+            this.ServerMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.Server_ConnectMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.Server_DisconnectMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateBackup_MI = new System.Windows.Forms.ToolStripMenuItem();
+            this.InstallBackup_MI = new System.Windows.Forms.ToolStripMenuItem();
+            this.HelpMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.Help_ContentsMI = new System.Windows.Forms.ToolStripMenuItem();
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.BrowseCTRL = new Opc.Ua.Client.Controls.BrowseNodeCtrl();
+            this.ConnectServerCTRL = new Opc.Ua.Client.Controls.ConnectServerCtrlbyIP();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.MenuBar.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MenuBar
+            // 
+            this.MenuBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ServerMI,
+            this.backupToolStripMenuItem,
+            this.HelpMI});
+            this.MenuBar.Location = new System.Drawing.Point(0, 0);
+            this.MenuBar.Name = "MenuBar";
+            this.MenuBar.Size = new System.Drawing.Size(1179, 28);
+            this.MenuBar.TabIndex = 1;
+            this.MenuBar.Text = "menuStrip1";
+            // 
+            // ServerMI
+            // 
+            this.ServerMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Server_ConnectMI,
+            this.Server_DisconnectMI});
+            this.ServerMI.Name = "ServerMI";
+            this.ServerMI.Size = new System.Drawing.Size(64, 24);
+            this.ServerMI.Text = "Server";
+            // 
+            // Server_ConnectMI
+            // 
+            this.Server_ConnectMI.Name = "Server_ConnectMI";
+            this.Server_ConnectMI.Size = new System.Drawing.Size(224, 26);
+            this.Server_ConnectMI.Text = "Connect";
+            this.Server_ConnectMI.Click += new System.EventHandler(this.Server_ConnectMI_ClickAsync);
+            // 
+            // Server_DisconnectMI
+            // 
+            this.Server_DisconnectMI.Name = "Server_DisconnectMI";
+            this.Server_DisconnectMI.Size = new System.Drawing.Size(224, 26);
+            this.Server_DisconnectMI.Text = "Disconnect";
+            this.Server_DisconnectMI.Click += new System.EventHandler(this.Server_DisconnectMI_Click);
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateBackup_MI,
+            this.InstallBackup_MI});
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.backupToolStripMenuItem.Text = "Backup";
+            // 
+            // CreateBackup_MI
+            // 
+            this.CreateBackup_MI.Name = "CreateBackup_MI";
+            this.CreateBackup_MI.Size = new System.Drawing.Size(224, 26);
+            this.CreateBackup_MI.Text = "Create Backup";
+            this.CreateBackup_MI.Click += new System.EventHandler(this.CreateBackup_MI_Click);
+            // 
+            // InstallBackup_MI
+            // 
+            this.InstallBackup_MI.Name = "InstallBackup_MI";
+            this.InstallBackup_MI.Size = new System.Drawing.Size(224, 26);
+            this.InstallBackup_MI.Text = "Install";
+            this.InstallBackup_MI.Click += new System.EventHandler(this.InstallBackup_MI_Click);
+            // 
+            // HelpMI
+            // 
+            this.HelpMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Help_ContentsMI});
+            this.HelpMI.Name = "HelpMI";
+            this.HelpMI.Size = new System.Drawing.Size(55, 24);
+            this.HelpMI.Text = "Help";
+            // 
+            // Help_ContentsMI
+            // 
+            this.Help_ContentsMI.Name = "Help_ContentsMI";
+            this.Help_ContentsMI.Size = new System.Drawing.Size(150, 26);
+            this.Help_ContentsMI.Text = "Contents";
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StatusBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.StatusBar.Location = new System.Drawing.Point(0, 710);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.StatusBar.Size = new System.Drawing.Size(1179, 20);
+            this.StatusBar.TabIndex = 2;
+            // 
+            // BrowseCTRL
+            // 
+            this.BrowseCTRL.AttributesListCollapsed = false;
+            this.BrowseCTRL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowseCTRL.Location = new System.Drawing.Point(0, 30);
+            this.BrowseCTRL.Margin = new System.Windows.Forms.Padding(0);
+            this.BrowseCTRL.Name = "BrowseCTRL";
+            this.BrowseCTRL.Size = new System.Drawing.Size(1179, 680);
+            this.BrowseCTRL.SplitterDistance = 387;
+            this.BrowseCTRL.TabIndex = 0;
+            this.BrowseCTRL.View = null;
+            // 
+            // ConnectServerCTRL
+            // 
+            this.ConnectServerCTRL.Configuration = null;
+            this.ConnectServerCTRL.DisableDomainCheck = false;
+            this.ConnectServerCTRL.DiscoverTimeout = 15000;
+            this.ConnectServerCTRL.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConnectServerCTRL.Location = new System.Drawing.Point(0, 0);
+            this.ConnectServerCTRL.Margin = new System.Windows.Forms.Padding(0);
+            this.ConnectServerCTRL.MaximumSize = new System.Drawing.Size(2731, 28);
+            this.ConnectServerCTRL.MinimumSize = new System.Drawing.Size(667, 28);
+            this.ConnectServerCTRL.Name = "ConnectServerCTRL";
+            this.ConnectServerCTRL.PreferredLocales = null;
+            this.ConnectServerCTRL.ReconnectPeriod = 10;
+            this.ConnectServerCTRL.ServerUrl = "opc.tcp://opc.tcp://opc.tcp://opc.tcp://opc.tcp://opc.tcp://opc.tcp://opc.tcp://o" +
+    "pc.tcp://opc.tcp://:4840:4840:4840:4840:4840:4840:4840";
+            this.ConnectServerCTRL.SessionName = null;
+            this.ConnectServerCTRL.SessionTimeout = ((uint)(60000u));
+            this.ConnectServerCTRL.Size = new System.Drawing.Size(1179, 28);
+            this.ConnectServerCTRL.StatusStrip = this.StatusBar;
+            this.ConnectServerCTRL.TabIndex = 4;
+            this.ConnectServerCTRL.UserIdentity = null;
+            this.ConnectServerCTRL.ReconnectStarting += new System.EventHandler(this.Server_ReconnectStarting);
+            this.ConnectServerCTRL.ReconnectComplete += new System.EventHandler(this.Server_ReconnectComplete);
+            this.ConnectServerCTRL.ConnectComplete += new System.EventHandler(this.Server_ConnectComplete);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.BrowseCTRL, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.StatusBar, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ConnectServerCTRL, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1179, 730);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 758);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.MenuBar);
+            this.MainMenuStrip = this.MenuBar;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "BR Utility";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.MenuBar.ResumeLayout(false);
+            this.MenuBar.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuBar;
+        private System.Windows.Forms.StatusStrip StatusBar;
+        private System.Windows.Forms.ToolStripMenuItem ServerMI;
+        private System.Windows.Forms.ToolStripMenuItem Server_ConnectMI;
+        private System.Windows.Forms.ToolStripMenuItem Server_DisconnectMI;
+        private System.Windows.Forms.ToolStripMenuItem HelpMI;
+        private System.Windows.Forms.ToolStripMenuItem Help_ContentsMI;
+        private Opc.Ua.Client.Controls.ConnectServerCtrlbyIP ConnectServerCTRL;
+        private Opc.Ua.Client.Controls.BrowseNodeCtrl BrowseCTRL;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateBackup_MI;
+        private System.Windows.Forms.ToolStripMenuItem InstallBackup_MI;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
